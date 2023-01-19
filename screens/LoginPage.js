d5cf34f142386295a57f0a5e89be8b39d3b17195
Oshadhi Pabasara Kumari Ekanayaka <98898738+Oshadhi2github@users.noreply.github.com>
2023-01-19 21:27:58 +0530

    create and rotate squares

--- screens/LoginPage.js
+++ screens/LoginPage.js
@@ -4,7 +4,13 @@ import {View, Text, StyleSheet} from "react-native";
 export default function Login(){
     return(
         <View style={styles.container}>
-            <View style={styles.top}></View>
+            <View style={styles.top}>
+                <View style={styles.v2}></View>
+                <View style={styles.v1}>
+
+                </View>
+
+            </View>
             <View style={styles.middle}></View>
             <View style={styles.bottom}></View>
         </View>
@@ -30,5 +36,21 @@ const styles= StyleSheet.create({
         height:50,
         borderWidth:1,
         borderColor:'red'
+    },
+    v1:{
+        width:150,
+        height:100,
+        borderColor:'red',
+        borderWidth:1,
+        transform:[{rotate:'-35deg'}],
+        borderRadius:5
+    },
+    v2:{
+        width:200,
+        height:150,
+        borderColor:'red',
+        borderWidth:1,
+        transform:[{rotate:'-35deg'}],
+        borderRadius:5
     }
 })
