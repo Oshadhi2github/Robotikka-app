7737779ac6b22c0adcad6f61695f81e81bbfb06a
Oshadhi Pabasara Kumari Ekanayaka <98898738+Oshadhi2github@users.noreply.github.com>
2023-01-27 11:08:57 +0530

    create a LandingPage and navigation part

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import LoginPage from './screens/LoginPage';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import SignUpPage from "./screens/SignUpPage";
+import LandingPage from "./screens/LandingPage";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export default function App(){
             <Stack.Navigator>
                 <Stack.Screen name="Login" component={LoginPage} options={{headerShown:false}}/>
                 <Stack.Screen name="Signup" component={SignUpPage} options={{headerShown:false}}/>
+                <Stack.Screen name="LandingPage" component={LandingPage} options={{headerShown:true}}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
