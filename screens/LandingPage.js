df7325d46e19d6e89570db516db214726c7dcdaf
Oshadhi Pabasara Kumari Ekanayaka <98898738+Oshadhi2github@users.noreply.github.com>
2023-01-28 12:10:33 +0530

    create context part and add images.

--- screens/LandingPage.js
+++ screens/LandingPage.js
@@ -1,9 +1,26 @@
-import {View,ScrollView,Text, StyleSheet} from 'react-native';
+import {View,ScrollView,ImageBackground,Text, StyleSheet} from 'react-native';
 export default function LandingPage({navigation}){
     return(
         <ScrollView style={styles.container}>
             <View style={styles.card}>
                 <View style={styles.cardInner}></View>
+            </View>
+
+            <Text style={styles.bestSellingText}>Best Selling</Text>
+
+            <View style={styles.context}>
+                <View style={styles.contextInner}>
+                    <View style={styles.dataInner}>
+                        <ImageBackground source='https://img.freepik.com/free-psd/two-seater-gray-sofa-with-two-cushions-isolated_176382-98.jpg?w=900&t=st=1674884392~exp=1674884992~hmac=f57885cc3cde93fac2912cec84f5988082b8421e90bd502a439e4c3d1958d395' style={styles.image}></ImageBackground>
+                    </View>
+                    <View style={styles.iconBox}></View>
+                </View>
+                <View style={styles.contextInner}>
+                    <View style={styles.dataInner}>
+                        <ImageBackground source='https://img.freepik.com/free-psd/two-seater-gray-sofa-with-two-cushions-isolated_176382-98.jpg?w=900&t=st=1674884392~exp=1674884992~hmac=f57885cc3cde93fac2912cec84f5988082b8421e90bd502a439e4c3d1958d395' style={styles.image}></ImageBackground>
+                    </View>
+                    <View style={styles.iconBox}></View>
+                </View>
             </View>
         </ScrollView>
     )
@@ -33,5 +50,47 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
+    },
+    bestSellingText:{
+        color:'#c0392b',
+        paddingLeft:10,
+        fontSize:20
+    },
+    context:{
+        height:'auto',
+        padding:5,
+        flexDirection:'row'
+    },
+    contextInner:{
+        width:'50%',
+        height:200,
+        padding:10
+    },
+    iconBox:{
+        width:35,
+        height:35,
+        backgroundColor:'#1abc9c',
+        borderRadius:4,
+        position:'absolute',
+        right:0,
+        bottom:0
+    },
+    dataInner:{
+        width:'100%',
+        height:'100%',
+        shadowColor: "#000",
+        borderRadius:3,
+        shadowOffset:{
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    image:{
+        width:'100%',
+        height:80
     }
+
 })
